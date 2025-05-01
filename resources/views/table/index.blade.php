75c0d9c91502787d7004063ad20335f3fd920f2d
iceeee <kenzo.lay1936@gmail.com>
2025-05-01 14:07:07 +0700

    first commit

---
+++ resources/views/table/index.blade.php
@@ -0,0 +1,184 @@
+@extends('admin.layout')
+@section('style')
+<link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.css" />
+@endsection
+@section('content')
+<div class="content-wrapper">
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-end">
+                        <a href="{{route('tableCreate')}}" class="btn btn-sm btn-outline-success d-flex align-items-center" style="font-size:14px">เพิ่มโต้ะ&nbsp;<i class="bx bxs-plus-circle"></i></a>
+                    </div>
+                    <div class="card-body">
+                        <table id="myTable" class="display" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th class="text-center">เลขโต้ะ</th>
+                                    <th class="text-center">QR-Code</th>
+                                    <th class="text-center">จัดการ</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" id="modal-qr">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">QR-code</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="card">
+                    <div class="card-body d-flex justify-content-center">
+                        <div class="row">
+                            <div class="col-12 d-flex justify-content-center mb-3" id="body-html">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success" id="download" data-id="">ดาวน์โหลด</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.datatables.net/2.2.2/js/dataTables.js"></script>
+<script>
+    var language = '{{asset("assets/js/datatable-language.js")}}';
+    $(document).ready(function() {
+        $("#myTable").DataTable({
+            language: {
+                url: language,
+            },
+            processing: true,
+            ajax: {
+                url: "{{route('tablelistData')}}",
+                type: "post",
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                },
+            },
+
+            columns: [{
+                    data: 'number',
+                    class: 'text-center',
+                    width: '60%'
+                },
+                {
+                    data: 'qr_code',
+                    class: 'text-center',
+                    width: '20%',
+                    orderable: false
+                },
+                {
+                    data: 'action',
+                    class: 'text-center',
+                    width: '20%',
+                    orderable: false
+                },
+            ]
+        });
+    });
+</script>
+<script>
+    $(document).on('click', '.deleteTable', function(e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        Swal.fire({
+            title: "ท่านต้องการลบโต้ะใช่หรือไม่?",
+            icon: "question",
+            showDenyButton: true,
+            confirmButtonText: "ตกลง",
+            denyButtonText: `ยกเลิก`
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: "{{route('tableDelete')}}",
+                    type: "post",
+                    data: {
+                        id: id
+                    },
+                    headers: {
+                        'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                    },
+                    success: function(response) {
+                        if (response.status == true) {
+                            Swal.fire(response.message, "", "success");
+                            $('#myTable').DataTable().ajax.reload(null, false);
+                        } else {
+                            Swal.fire(response.message, "", "error");
+                        }
+                    }
+                });
+            }
+        });
+    });
+    $(document).on('click', '.modalQr', function(e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        $.ajax({
+            type: "post",
+            url: "{{route('QRshow')}}",
+            data: {
+                id: id
+            },
+            headers: {
+                'X-CSRF-TOKEN': '{{ csrf_token() }}'
+            },
+            success: function(response) {
+                $('#modal-qr').modal('show')
+                $('#download').attr('data-id', id);
+                $('#body-html').html(response);
+            }
+        });
+    });
+
+    $(document).on('click', '#download', function(e) {
+        e.preventDefault();
+
+        const svgElement = document.querySelector('#body-html svg');
+        if (!svgElement) {
+            alert('QR code SVG not found!');
+            return;
+        }
+
+        const svgData = new XMLSerializer().serializeToString(svgElement);
+        const svgBlob = new Blob([svgData], {
+            type: 'image/svg+xml;charset=utf-8'
+        });
+        const url = URL.createObjectURL(svgBlob);
+
+        const img = new Image();
+        img.onload = function() {
+            const canvas = document.createElement('canvas');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+
+            const pngFile = canvas.toDataURL('image/png');
+            const downloadLink = document.createElement('a');
+            downloadLink.download = 'qr_code.png';
+            downloadLink.href = pngFile;
+            downloadLink.click();
+
+            URL.revokeObjectURL(url);
+        };
+        img.src = url;
+    });
+</script>
+@endsection
